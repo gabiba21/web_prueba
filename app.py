11df479be9483a286defae16b48ab3fec3de297c
gabo203 <ga.ibanez@duocuc.cl>
2025-06-08 15:07:45 -0400

    Fix deployment for Render

--- app.py
+++ app.py
@@ -28,4 +28,5 @@ def productos():
     return render_template("productos.html", productos=data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
